99ac1022573b778425c3b4b0881c7d179b0c0f4e
vvinnych-regrow <vitalii@mealhow.ai>
2024-01-01 19:25:35 +0200

    use default datastore database

--- meal-plan-and-images-generator/clients.py
+++ meal-plan-and-images-generator/clients.py
@@ -2,6 +2,6 @@ import config
 from google.cloud import ndb
 from mealhow_sdk.clients import CloudStorage, HttpClient
 
-ndb_client = ndb.Client(database=config.DATASTORE_DB)
+ndb_client = ndb.Client()
 cloud_storage_session = CloudStorage()
 http_client = HttpClient()
